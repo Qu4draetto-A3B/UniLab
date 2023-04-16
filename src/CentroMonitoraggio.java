220626c0a541934aa8b81509c18fbcf1df513531
Michael-Bernasconi <mikeberna.mb@gmail.com>
2023-04-16 21:08:51 +0200

    Update CentroMonitoraggio.java

--- src/CentroMonitoraggio.java
+++ src/CentroMonitoraggio.java
@@ -1,6 +1,7 @@
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import javax.print.attribute.standard.Sides;
@@ -58,9 +59,10 @@ public class CentroMonitoraggio {
 					record.get("CountryName"), record.get("Name"));
 			lag.addFirst(ag);
 		}
-		System.out.print("inserire: \n 1)cerca area per denominazione: \n 2)cerca aree per coordinate : \n per uscire qualsiasi altro valore: \n");
+		System.out.print(
+				"inserire: \n 1)cerca area per denominazione: \n 2)cerca aree per coordinate : \n per uscire qualsiasi altro valore: \n");
 		int caso = sc.nextInt();
-		sc.next();
+		sc.nextLine();
 		do {
 			switch (caso) {
 				case 1:
@@ -71,48 +73,61 @@ public class CentroMonitoraggio {
 					for (AreaGeografica tmp : listatmp)
 						System.out.println("" + ++i + " " + tmp.toString());
 
-					
-					do
-					{
-						System.out.print("Inserire l'area da aggiungere nel centro di monitoraggio (inserire valore compreso tra 1 e " +listatmp.size()+") \n");
+					do {
+						System.out.print(
+								"Inserire l'area da aggiungere nel centro di monitoraggio (inserire valore compreso tra 1 e "
+										+ listatmp.size() + ") \n");
 						i = sc.nextInt();
 						sc.nextLine();
-					}while((i <= 0 )||(i > listatmp.size()));
-					
+					} while ((i <= 0) || (i > listatmp.size()));
 
 					this.aree.addFirst(listatmp.get(i - 1));
 
 					break;
 
 				case 2: // se cerco con le coordinate mi resituisce l'area geografica
-						System.out.print("Inserire la latitudine : ");
-						double lat = sc.nextDouble();
+					System.out.print("Inserire la latitudine : ");
+					double lat;
+					try {
+						lat = sc.nextDouble();
 						sc.nextLine();
+					} catch (InputMismatchException e) {
+						e.printStackTrace();
+						continue;
+					}
 
-						System.out.print("Inserire la longitudine : ");
-						double lon = sc.nextDouble();
+
+					System.out.print("Inserire la longitudine : ");
+					double lon;
+					try {
+						lon = sc.nextDouble();
 						sc.nextLine();
+					} catch (InputMismatchException e) {
+						e.printStackTrace();
+						continue;
+					}
 
-						ag = lag.cercaAreeGeografiche(lat, lon).get();
-						System.out.println("Area trovata: " +ag.toString());
+					ag = lag.cercaAreeGeografiche(lat, lon).get();
+					System.out.println("Area trovata: " + ag.toString());
 
-						System.out.println("Vuoi aggiungere l'area al centro di monitoraggio? \n inserire s per si \n inserire n per no \n");
-						String r = sc.nextLine();
-						if(r.toLowerCase().equals("s"))
-						{
-							this.aree.addFirst(ag);
-							System.out.println("area aggiunta");
-						}
-						else
-							System.out.println("area non aggiunta");
+					System.out.println(
+							"Vuoi aggiungere l'area al centro di monitoraggio? \n inserire s per si \n inserire n per no \n");
+					String r = sc.nextLine();
+					if (r.toLowerCase().equals("s")) {
+						this.aree.addFirst(ag);
+						System.out.println("area aggiunta");
+					} else
+						System.out.println("area non aggiunta");
 
-						break;					
+					break;
 
 				default:
-					if(this.aree.size() > 0) return;
+					if (this.aree.size() > 0)
+						return;
 
 			}
-			System.out.print("inserire: \n 1)cerca area per denominazione: \n 2)cerca aree per coordinate : \n per uscire qualsiasi altro valore: \n");
+			System.out.print(
+					"inserire: \n 1)cerca area per denominazione: \n 2)cerca aree per coordinate : \n per uscire qualsiasi altro valore: \n");
 			caso = sc.nextInt();
 			sc.nextLine();
 
