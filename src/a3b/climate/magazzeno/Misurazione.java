13274e4b3ed0291dcb780417710f19a08dc4936b
beatricebalzarini <beba.balzarini@gmail.com>
2023-04-21 11:32:55 +0200

    scheletro Misurazione

--- src/a3b/climate/magazzeno/Misurazione.java
+++ src/a3b/climate/magazzeno/Misurazione.java
@@ -1,5 +1,38 @@
 package a3b.climate.magazzeno;
 
+import java.time.LocalDateTime;
+
 public class Misurazione {
-	//TODO
+	private DatoGeografico dato;
+	private LocalDateTime time;
+	private Operatore operatore;
+	private CentroMonitoraggio centro;
+	private AreaGeografica area;
+
+	public Misurazione(DatoGeografico dato, Operatore operatore, AreaGeografica area) {
+		this.dato = dato;
+		this.operatore = operatore;
+		this.area = area;
+	}
+
+	public DatoGeografico getDato() {
+		return this.dato;
+	}
+
+	public LocalDateTime getTime() {
+		return this.time;
+	}
+
+	public Operatore getOperatore() {
+		return this.operatore;
+	}
+
+	public CentroMonitoraggio getCentroMonitoraggio() {
+		return this.centro;
+	}
+
+	public AreaGeografica getArea() {
+		return this.area;
+	}
+
 }
