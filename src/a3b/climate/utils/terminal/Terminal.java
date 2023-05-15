9be1c0d9f35240bfb00b4d9e7cd695ed13ec624f
Iuri <iuricube@gmail.com>
2023-05-15 09:43:14 +0200

    Header su tutti i file

--- src/a3b/climate/utils/terminal/Terminal.java
+++ src/a3b/climate/utils/terminal/Terminal.java
@@ -1,3 +1,17 @@
+/*
+ * Interdisciplinary Workshop A
+ * Climate Monitoring
+ * A.A. 2022-2023
+ *
+ * Authors:
+ * - Iuri Antico, 753144
+ * - Beatrice Balzarini, 752257
+ * - Michael Bernasconi, 752259
+ * - Gabriele Borgia, 753262
+ *
+ * Some rights reserved.
+ * See LICENSE file for additional information.
+ */
 package a3b.climate.utils.terminal;
 
 import java.io.Console;
