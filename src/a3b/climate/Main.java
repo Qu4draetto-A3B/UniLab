9417108523803a5ad4e6a94f57df151b484f7979
Iuri <iuricube@gmail.com>
2023-05-20 11:29:29 +0200

    Opzioni linea di comando
    Classe per lettura file INI

--- src/a3b/climate/Main.java
+++ src/a3b/climate/Main.java
@@ -14,6 +14,7 @@
  */
 package a3b.climate;
 
+import a3b.climate.cli.App;
 import a3b.climate.utils.terminal.Screen;
 import org.apache.commons.cli.*;
 
@@ -33,23 +34,91 @@ public class Main {
 	public static Screen scn = new Screen();
 	public static CommandLine line;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		Options opts = new Options();
 		opts.addOption(Option.builder("u")
 			.argName("utente")
 			.longOpt("utente")
 			.desc("Nome utente e password, separati da virgola")
+			.required(true)
 			.valueSeparator(',')
 			.numberOfArgs(2)
 			.optionalArg(false)
 			.build());
 
 		opts.addOption(Option.builder("g")
-			.argName("gettone")
+			.argName("gettone utente")
 			.longOpt("gettone")
 			.desc("File con nome utente e password nel formato 'utente,password'")
+			.required(true)
 			.hasArg()
 			.optionalArg(false)
+			.build());
+
+		opts.addOption(Option.builder()
+			.argName("avvia")
+			.longOpt("avvia-registrazione")
+			.desc("Avvia il processo di registrazione")
+			.build());
+
+		opts.addOption(Option.builder("q")
+			.argName("avvia")
+			.longOpt("query")
+			.desc("Avvia un'operazione di ricerca dati")
+			.hasArg()
+			.optionalArg(true)
+			.build());
+
+		opts.addOption(Option.builder()
+			.argName("parole")
+			.longOpt("lista-aree")
+			.desc("Avvia un'operazione di ricerca sulle aree geografiche")
+			.hasArgs()
+			.valueSeparator(',')
+			.optionalArg(true)
+			.build());
+
+		opts.addOption(Option.builder()
+			.argName("parole")
+			.longOpt("lista-centri")
+			.desc("Avvia un'operazione di ricerca sui centri di monitoraggio")
+			.hasArgs()
+			.valueSeparator(',')
+			.optionalArg(true)
+			.build());
+
+		opts.addOption(Option.builder()
+			.argName("parole")
+			.longOpt("lista-misurazioni")
+			.desc("Avvia un'operazione di ricerca sui centri di monitoraggio")
+			.hasArgs()
+			.valueSeparator(',')
+			.optionalArg(true)
+			.build());
+
+		opts.addOption(Option.builder()
+			.argName("parole")
+			.longOpt("lista-operatori")
+			.desc("Avvia un'operazione di ricerca sui centri di monitoraggio")
+			.hasArgs()
+			.valueSeparator(',')
+			.optionalArg(true)
+			.build());
+
+		opts.addOption(Option.builder()
+			.argName("avvia")
+			.longOpt("crea-centro")
+			.desc("Avvia la creazione di un centro")
+			.hasArg()
+			.optionalArg(true)
+			.build());
+
+		opts.addOption(Option.builder()
+			.argName("avvia")
+			.longOpt("crea-misurazione")
+			.desc("Avvia la creazione di una misurazione")
+			.hasArg()
+			.optionalArg(true)
 			.build());
 
 		if (args.length < 1) {
@@ -58,10 +127,18 @@ public class Main {
 				"Programma per il monitoraggio climatico",
 				opts,
 				"Leggi il Manuale Utente per informazioni aggiuntive",
-				true);
+				false);
+			return;
 		}
 
 		DefaultParser par = new DefaultParser();
-		line = par.parse(opts, args);
+		try {
+			line = par.parse(opts, args);
+		} catch (ParseException e) {
+			System.out.println(e.getMessage());
+			return;
+		}
+
+		App.start(line);
 	}
 }
---
+++ src/a3b/climate/cli/App.java
@@ -0,0 +1,9 @@
+package a3b.climate.cli;
+
+import org.apache.commons.cli.CommandLine;
+
+public class App {
+	public static void start(CommandLine line) {
+		//
+	}
+}
